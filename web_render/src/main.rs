bf374d5829d99f1b0b5e0d1f411480fa9ad24d57
dannyxy <32197801+danny2604@users.noreply.github.com>
2024-01-31 19:28:43 +0100

    Remove old web_render workspace

--- web_render/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod components;
-use components::app::App;
-
-fn main() {
-    let _ = console_log::init_with_level(log::Level::Debug);
-    console_error_panic_hook::set_once();
-    leptos::mount_to_body(App);
-}
